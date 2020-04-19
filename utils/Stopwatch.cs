d36f7cf04867ed0cd3157c7e1d8a9ed6f76bfe9e
Isaac <ijsaffold@mail.lipscomb.edu>
2020-04-19 01:59:14 -0500

    created Unity stopwatch

---
+++ utils/Stopwatch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace ProjectZombie
+{
+    public class Stopwatch: MonoBehaviour
+    {
+        public float ElapsedSeconds {get; private set;} = 0;
+
+        void Update() => ElapsedSeconds += Time.deltaTime;
+    }
+}
